5def60b0fd886e14d877f2eb2c15a759f864f65d
jspearritt <jspearritt@atlassian.com>
2021-12-12 18:35:56 +1100

    Day11 and 12 skeletons

---
+++ scala/aoc21/src/main/scala/aoc21/day11/Day11.scala
@@ -0,0 +1,16 @@
+package aoc21.day11
+
+import aoc21.common.SolutionWithParser
+import cats.parse.Parser
+
+object Day11 extends SolutionWithParser[String, Int] {
+  override def dayNumber: Int = 11
+
+  override def parser: Parser[String] = ???
+
+  override def solvePart1(input: String): Int = ???
+
+  override def solvePart2(input: String): Int = ???
+}
+
+@main def run = Day11.testSolution()
---
+++ scala/aoc21/src/main/scala/aoc21/day12/Day12.scala
@@ -0,0 +1,16 @@
+package aoc21.day12
+
+import aoc21.common.SolutionWithParser
+import cats.parse.Parser
+
+object Day12 extends SolutionWithParser[String, Int] {
+  override def dayNumber: Int = 11
+
+  override def parser: Parser[String] = ???
+
+  override def solvePart1(input: String): Int = ???
+
+  override def solvePart2(input: String): Int = ???
+}
+
+@main def run = Day12.testSolution()
