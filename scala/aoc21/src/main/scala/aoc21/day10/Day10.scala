fdfa09269da566054f83cc5d27c0af12922564f9
jspearritt <jspearritt@atlassian.com>
2021-12-12 18:18:54 +1100

    Day10 skeleton

---
+++ scala/aoc21/src/main/scala/aoc21/day10/Day10.scala
@@ -0,0 +1,18 @@
+package aoc21.day10
+
+import aoc21.common.SolutionWithParser
+import cats.parse.Parser
+
+
+
+object Day10 extends SolutionWithParser[String, Int] {
+  override def dayNumber: Int = 10
+
+  override def parser: Parser[String] = ???
+
+  override def solvePart1(input: String): Int = ???
+
+  override def solvePart2(input: String): Int = ???
+}
+
+@main def run = Day10.testSolution()
