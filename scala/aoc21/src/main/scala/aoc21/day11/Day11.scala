731f0129cca12abb2833ae5f006370ca3d4d94e0
Joseph Spearritt <joe.spearritt@live.com>
2021-12-13 22:58:29 +1100

    Day11 parsing

--- scala/aoc21/src/main/scala/aoc21/day11/Day11.scala
+++ scala/aoc21/src/main/scala/aoc21/day11/Day11.scala
@@ -1,16 +1,23 @@
 package aoc21.day11
 
-import aoc21.common.SolutionWithParser
-import cats.parse.Parser
+import aoc21.common.{CommonParsers, SolutionWithParser}
+import cats.parse.{Numbers, Parser}
 
-object Day11 extends SolutionWithParser[String, Int] {
+object Parsing:
+  val octopusEnergy: Parser[Int] = Numbers.digit.map(_.toInt - '0')
+
+  val octopusLine: Parser[Array[Int]] = octopusEnergy.rep(10, 10).map(l => Array(l.toList:_*))
+
+  val octopusArray: Parser[Array[Array[Int]]] = CommonParsers.lineSeparated(octopusLine).map(l => Array(l:_*))
+
+object Day11 extends SolutionWithParser[Array[Array[Int]], Int] {
   override def dayNumber: Int = 11
 
-  override def parser: Parser[String] = ???
+  override def parser: Parser[Array[Array[Int]]] = Parsing.octopusArray
 
-  override def solvePart1(input: String): Int = ???
+  override def solvePart1(input: Array[Array[Int]]): Int = ???
 
-  override def solvePart2(input: String): Int = ???
+  override def solvePart2(input: Array[Array[Int]]): Int = ???
 }
 
 @main def run = Day11.testSolution()
