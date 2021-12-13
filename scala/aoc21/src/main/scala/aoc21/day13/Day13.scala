8df97ed4a49b3a575de15444617424e28d474efb
Joseph Spearritt <joe.spearritt@live.com>
2021-12-13 21:55:45 +1100

    Complete day13

--- scala/aoc21/src/main/scala/aoc21/day13/Day13.scala
+++ scala/aoc21/src/main/scala/aoc21/day13/Day13.scala
@@ -2,6 +2,7 @@ package aoc21.day13
 
 import aoc21.common.{CommonParsers, SolutionWithParser}
 import cats.parse.Parser
+import scala.collection.mutable.StringBuilder
 
 case class Dot(x: Int, y: Int)
 
@@ -32,16 +33,49 @@ object Parsing {
     Page(dots, folds)
 }
 
+def visualise(dots: List[Dot]): String =
+  val maxX = dots.maxBy { case Dot(x, y) => x }.x
+  val maxY = dots.maxBy { case Dot(x, y) => y }.y
+  val arr: Array[Array[Boolean]] = Array.ofDim(maxY + 1, maxX + 1)
+  for Dot(x, y) <- dots do
+    arr(y).update(x, true)
+
+  val sb = StringBuilder()
+  for y <- arr.indices do
+    for x <- arr(y).indices do
+      sb.addOne(if arr(y)(x) then '#' else '.')
+    sb.addOne('\n')
+
+  sb.mkString
+
+def applyFold(dots: List[Dot], fold: FoldInstruction): List[Dot] =
+  if fold.axis == Axis.X then
+    dots.map {
+      case Dot(x, y) if x < fold.pos => Dot(x, y)
+      case Dot(x, y) if x > fold.pos => Dot(fold.pos - (x - fold.pos), y)
+    }.distinct
+  else
+    dots.map {
+      case Dot(x, y) if y < fold.pos => Dot(x, y)
+      case Dot(x, y) if y > fold.pos => Dot(x, fold.pos - (y - fold.pos))
+    }.distinct
+
+def getFirstDots(page: Page): List[Dot] = applyFold(page.dots, page.folds(0))
+
+def getFinalDots(page: Page): List[Dot] = page.folds.foldLeft(page.dots) { case (dots, fold) => applyFold(dots, fold) }
+
+
 object Day13 extends SolutionWithParser[Page, Int]:
   override def dayNumber: Int = 13
 
   override def parser: Parser[Page] = Parsing.page
 
   override def solvePart1(input: Page): Int =
-    println(input)
-    ???
+    getFirstDots(input).length
 
-  override def solvePart2(input: Page): Int = ???
+  override def solvePart2(input: Page): Int =
+    println(visualise(getFinalDots(input)))
+    0
 
 
-@main def run = Day13.testSolution()
+@main def run = Day13.runSolution
