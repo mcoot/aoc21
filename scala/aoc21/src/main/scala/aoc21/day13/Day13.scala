b4a6a4cc4707bd03bdd71f3b562083b70092d3cd
Joseph Spearritt <joe.spearritt@live.com>
2021-12-13 21:15:36 +1100

    Day13 parsing

--- scala/aoc21/src/main/scala/aoc21/day13/Day13.scala
+++ scala/aoc21/src/main/scala/aoc21/day13/Day13.scala
@@ -1,16 +1,47 @@
 package aoc21.day13
 
-import aoc21.common.SolutionWithParser
+import aoc21.common.{CommonParsers, SolutionWithParser}
 import cats.parse.Parser
 
-object Day13 extends SolutionWithParser[String, Int]:
+case class Dot(x: Int, y: Int)
+
+enum Axis:
+  case X
+  case Y
+
+case class FoldInstruction(axis: Axis, pos: Int)
+
+case class Page(dots: List[Dot], folds: List[FoldInstruction])
+
+object Parsing {
+  val dot = ((CommonParsers.int <* Parser.char(',')) ~ CommonParsers.int).map { case (x, y) => Dot(x, y) }
+
+  val axis = Parser.charIn(List('x', 'y')).map { _ match
+    case 'x' => Axis.X
+    case 'y' => Axis.Y
+  }
+
+  val fold = Parser.string("fold along ") *> ((axis <* Parser.char('=')) ~ CommonParsers.int)
+    .map { case (axis, pos) => FoldInstruction(axis, pos) }
+
+  val page = for
+    dots <- CommonParsers.lineSeparated(dot)
+    _ <- CommonParsers.newLine ~ CommonParsers.newLine
+    folds <- CommonParsers.lineSeparated(fold)
+  yield
+    Page(dots, folds)
+}
+
+object Day13 extends SolutionWithParser[Page, Int]:
   override def dayNumber: Int = 13
 
-  override def parser: Parser[String] = ???
+  override def parser: Parser[Page] = Parsing.page
 
-  override def solvePart1(input: String): Int = ???
+  override def solvePart1(input: Page): Int =
+    println(input)
+    ???
 
-  override def solvePart2(input: String): Int = ???
+  override def solvePart2(input: Page): Int = ???
 
 
 @main def run = Day13.testSolution()
