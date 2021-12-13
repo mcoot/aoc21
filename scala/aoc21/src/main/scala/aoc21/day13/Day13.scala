9e0fa5cff184e1997991df6bf59fed71287752bb
Joseph Spearritt <joe.spearritt@live.com>
2021-12-13 21:06:56 +1100

    Day13 skeleton

---
+++ scala/aoc21/src/main/scala/aoc21/day13/Day13.scala
@@ -0,0 +1,16 @@
+package aoc21.day13
+
+import aoc21.common.SolutionWithParser
+import cats.parse.Parser
+
+object Day13 extends SolutionWithParser[String, Int]:
+  override def dayNumber: Int = 13
+
+  override def parser: Parser[String] = ???
+
+  override def solvePart1(input: String): Int = ???
+
+  override def solvePart2(input: String): Int = ???
+
+
+@main def run = Day13.testSolution()
