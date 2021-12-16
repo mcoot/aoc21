c72749617068b2451f79e9df2ec039e2f93d223e
Joseph Spearritt <joe.spearritt@live.com>
2021-12-16 20:55:58 +1100

    Day16 parse a num-subpackets packet

--- scala/aoc21/src/main/scala/aoc21/day16/Day16.scala
+++ scala/aoc21/src/main/scala/aoc21/day16/Day16.scala
@@ -7,9 +7,14 @@ enum PacketType:
   case Literal
   case Operator(opCode: Int)
 
+enum PacketMode:
+  case BitLengthMode
+  case NumPacketsMode
 
 enum Packet(version: Int, packetType: PacketType):
   case Literal(version: Int, value: Int) extends Packet(version, PacketType.Literal)
+  case Operator(version: Int, opCode: Int, packets: List[Packet]) extends Packet(version, PacketType.Operator(opCode))
+
 
 object InputParsing:
   val hexDigitsToBits: Parser[String] = Rfc5234.hexdig.map { c =>
@@ -19,6 +24,7 @@ object InputParsing:
     String.format("%04d", intValue.toBinaryString.toInt)
   }.rep.map(_.toList.mkString)
 
+
 object PacketParsing:
   def bits(length: Int) = Parser.charIn(List('0', '1')).rep(length, length).map(_.toList.mkString)
 
@@ -27,8 +33,6 @@ object PacketParsing:
   val packetVersion: Parser[Int] = bitsAsInt(3)
 
   val literalPacketType: Parser[PacketType] = Parser.string("100").map(_ => PacketType.Literal)
-
-  val operatorPacketType: Parser[PacketType] = bitsAsInt(3).map(PacketType.Operator(_))
 
   val literalPacketSegmentContinue: Parser[Int] = Parser.char('1') *> bitsAsInt(4)
 
@@ -42,6 +46,41 @@ object PacketParsing:
     Packet.Literal(version, value)
   }
 
+  val operatorPacketType: Parser[PacketType.Operator] = bitsAsInt(3).map(PacketType.Operator(_))
+
+  val operatorPacketMode: Parser[PacketMode] = bits(1).map {
+    case "0" => PacketMode.BitLengthMode
+    case "1" => PacketMode.NumPacketsMode
+  }
+
+  val operatorPacketBitLength: Parser[Int] = bitsAsInt(15)
+
+  val operatorPacketBitLengthContents: Parser[List[Packet]] = for
+    bitLength <- operatorPacketBitLength
+  yield
+    ???
+
+  val operatorPacketNumPackets: Parser[Int] = bitsAsInt(11)
+
+  val operatorPacketNumPacketsContents: Parser[List[Packet]] = for
+    numPackets <- operatorPacketNumPackets
+    subPackets <- packet.rep(numPackets, numPackets).map(_.toList)
+  yield
+    subPackets
+
+  val operatorPacket: Parser[Packet] = for
+    version <- packetVersion
+    packetType <- operatorPacketType
+    mode <- operatorPacketMode
+    subPackets <- if mode == PacketMode.BitLengthMode then
+      operatorPacketBitLengthContents
+    else
+      operatorPacketNumPacketsContents
+  yield
+    Packet.Operator(version, packetType.opCode, subPackets)
+
+  val packet: Parser[Packet] = literalPacket.backtrack | operatorPacket
+
 
 object Day16 extends SolutionWithParser[String, Int]:
   override def dayNumber: Int = 16
@@ -49,12 +88,12 @@ object Day16 extends SolutionWithParser[String, Int]:
   override def parser: Parser[String] = InputParsing.hexDigitsToBits
 
   override def solvePart1(input: String): Int =
-    val parsed = PacketParsing.literalPacket.parse(input)
+    val parsed = PacketParsing.packet.parse(input)
     println(parsed)
     ???
 
   override def solvePart2(input: String): Int = ???
 
 
-@main def run = Day16.testSolution("a")
+@main def run = Day16.testSolution("c")
 
