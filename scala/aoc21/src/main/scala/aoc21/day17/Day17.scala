91e69a08a23d08c37d00bde89e38e9f28a8aa082
Joseph Spearritt <joe.spearritt@live.com>
2021-12-17 20:44:25 +1100

    Day 17 parsing

--- scala/aoc21/src/main/scala/aoc21/day17/Day17.scala
+++ scala/aoc21/src/main/scala/aoc21/day17/Day17.scala
@@ -1,6 +1,6 @@
 package aoc21.day17
 
-import aoc21.common.SolutionWithParser
+import aoc21.common.{CommonParsers, SolutionWithParser}
 import cats.parse.Parser
 
 
@@ -8,15 +8,25 @@ case class TargetAreaDefn(x1: Int, x2: Int, y1: Int, y2: Int)
 
 
 object Parsing:
-  ???
+  val range: Parser[(Int, Int)] = (CommonParsers.int <* Parser.string("..")) ~ CommonParsers.int
+
+  val targetAreaDefn: Parser[TargetAreaDefn] = for
+    _ <- Parser.string("target area: x=")
+    xRange <- range
+    _ <- Parser.string(", y=")
+    yRange <- range
+  yield
+    TargetAreaDefn(xRange(0), xRange(1), yRange(0), yRange(1))
 
 
 object Day17 extends SolutionWithParser[TargetAreaDefn, Int]:
   override def dayNumber: Int = 17
 
-  override def parser: Parser[TargetAreaDefn] = ???
+  override def parser: Parser[TargetAreaDefn] = Parsing.targetAreaDefn
 
-  override def solvePart1(input: TargetAreaDefn): Int = ???
+  override def solvePart1(input: TargetAreaDefn): Int =
+    println(input)
+    ???
 
   override def solvePart2(input: TargetAreaDefn): Int = ???
 
