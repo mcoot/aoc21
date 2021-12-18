c787fa6482f6aff6b0d16afd990b74d5b8fa4247
Joseph Spearritt <joe.spearritt@live.com>
2021-12-18 16:20:57 +1100

    Day 18 parsing

--- scala/aoc21/src/main/scala/aoc21/day18/Day18.scala
+++ scala/aoc21/src/main/scala/aoc21/day18/Day18.scala
@@ -1,21 +1,33 @@
 package aoc21.day18
 
-import aoc21.common.SolutionWithParser
+import aoc21.common.{CommonParsers, SolutionWithParser}
 import cats.parse.Parser
 
-case class SnailfishNumber()
+enum SnailfishNumber:
+  case Literal(value: Int)
+  case Pair(left: SnailfishNumber, right: SnailfishNumber)
 
 
 object Parsing:
-  ???
+  val literal: Parser[SnailfishNumber] = CommonParsers.int.map(SnailfishNumber.Literal(_))
+
+  val snailfishNumber: Parser[SnailfishNumber] = for
+    _ <- Parser.string("[")
+    left <- literal | snailfishNumber
+    _ <- Parser.string(",")
+    right <- literal | snailfishNumber
+    _ <- Parser.string("]")
+  yield
+    SnailfishNumber.Pair(left, right)
 
 
 object Day18 extends SolutionWithParser[List[SnailfishNumber], Int]:
   override def dayNumber: Int = 18
 
-  override def parser: Parser[List[SnailfishNumber]] = ???
+  override def parser: Parser[List[SnailfishNumber]] = CommonParsers.lineSeparated(Parsing.snailfishNumber)
 
-  override def solvePart1(input: List[SnailfishNumber]): Int = ???
+  override def solvePart1(input: List[SnailfishNumber]): Int =
+    ???
 
   override def solvePart2(input: List[SnailfishNumber]): Int = ???
 
