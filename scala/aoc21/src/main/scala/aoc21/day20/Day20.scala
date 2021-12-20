c08580b1829382ad3925bcbea56c8f03161842e4
Joseph Spearritt <joe.spearritt@live.com>
2021-12-20 20:36:06 +1100

    Day20 parsing update

--- scala/aoc21/src/main/scala/aoc21/day20/Day20.scala
+++ scala/aoc21/src/main/scala/aoc21/day20/Day20.scala
@@ -5,7 +5,7 @@ import cats.parse.Parser
 
 import scala.collection.mutable.Set as MutableSet
 
-case class Image(val activePixels: MutableSet[(Int, Int)])
+case class Image(activePixels: MutableSet[(Int, Int)], lowerBound: (Int, Int), upperBound: (Int, Int))
 
 case class Input(algorithm: Array[Boolean], initialImage: Image)
 
@@ -16,12 +16,20 @@ object Parsing:
   val algorithm: Parser[Array[Boolean]] = pixel.rep(512, 512).map(_.toList.toArray)
 
   val image: Parser[Image] = CommonParsers.lineSeparated(pixel.rep.map(_.toList)).map { rawImage =>
+    var minX = 0
+    var maxX = 0
+    var minY = 0
+    var maxY = 0
     val s: MutableSet[(Int, Int)] = MutableSet()
     for y <- rawImage.indices do
       for x <- rawImage(y).indices do
         if rawImage(y)(x) then
+          minX = Math.min(minX, x)
+          maxX = Math.max(maxX, x)
+          minY = Math.min(minY, y)
+          maxY = Math.max(maxY, y)
           s.add((x, y))
-    Image(s)
+    Image(s, (minX, minY), (maxX, maxY))
   }
 
   val input: Parser[Input] = for
