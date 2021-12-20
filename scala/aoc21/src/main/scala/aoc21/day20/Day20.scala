ce6852d36938cfa48cd15b3738204d2161aaefaa
Joseph Spearritt <joe.spearritt@live.com>
2021-12-20 20:30:18 +1100

    Day20 parsing

--- scala/aoc21/src/main/scala/aoc21/day20/Day20.scala
+++ scala/aoc21/src/main/scala/aoc21/day20/Day20.scala
@@ -1,18 +1,45 @@
 package aoc21.day20
 
-import aoc21.common.SolutionWithParser
+import aoc21.common.{CommonParsers, SolutionWithParser}
 import cats.parse.Parser
 
+import scala.collection.mutable.Set as MutableSet
 
-case class Input()
+case class Image(val activePixels: MutableSet[(Int, Int)])
+
+case class Input(algorithm: Array[Boolean], initialImage: Image)
+
+
+object Parsing:
+  val pixel: Parser[Boolean] = (Parser.char('#').map(_ => true)) | (Parser.char('.').map(_ => false))
+
+  val algorithm: Parser[Array[Boolean]] = pixel.rep(512, 512).map(_.toList.toArray)
+
+  val image: Parser[Image] = CommonParsers.lineSeparated(pixel.rep.map(_.toList)).map { rawImage =>
+    val s: MutableSet[(Int, Int)] = MutableSet()
+    for y <- rawImage.indices do
+      for x <- rawImage(y).indices do
+        if rawImage(y)(x) then
+          s.add((x, y))
+    Image(s)
+  }
+
+  val input: Parser[Input] = for
+    alg <- algorithm
+    _ <- CommonParsers.newLine ~ CommonParsers.newLine
+    img <- image
+  yield
+    Input(alg, img)
 
 
 object Day20 extends SolutionWithParser[Input, Int]:
   override def dayNumber: Int = 20
 
-  override def parser: Parser[Input] = ???
+  override def parser: Parser[Input] = Parsing.input
 
-  override def solvePart1(input: Input): Int = ???
+  override def solvePart1(input: Input): Int =
+    println(input)
+    ???
 
   override def solvePart2(input: Input): Int = ???
 
