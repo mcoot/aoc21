f28bd51dbe2da33d404ecb4647133d6be6a6b0c9
Joseph Spearritt <joe.spearritt@live.com>
2021-12-21 16:03:23 +1100

    Day 21 parsing

--- scala/aoc21/src/main/scala/aoc21/day21/Day21.scala
+++ scala/aoc21/src/main/scala/aoc21/day21/Day21.scala
@@ -1,16 +1,18 @@
 package aoc21.day21
 
-import aoc21.common.SolutionWithParser
+import aoc21.common.{CommonParsers, SolutionWithParser}
 import cats.parse.Parser
 
 object Parsing:
-  ???
+  val startingPos = (Parser.string("Player ") ~ CommonParsers.int ~ Parser.string(" starting position: ")) *> CommonParsers.int
+
+  val startingPosPair = (startingPos <* CommonParsers.newLine) ~ startingPos
 
 
 object Day21 extends SolutionWithParser[(Int, Int), Int]:
   override def dayNumber: Int = 21
 
-  override def parser: Parser[(Int, Int)] = ???
+  override def parser: Parser[(Int, Int)] = startingPosPair
 
   override def solvePart1(input: (Int, Int)): Int = ???
 
